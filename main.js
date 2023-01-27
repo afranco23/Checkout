2174c668815dd3c6b8adf577bbf4e9aef52d6077
Alexander De Franco <afranco231@gmail.com>
2023-01-27 14:48:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const selectCountry = document.querySelector("#countries")
+
+  fetch("https://restcountries.com/v2/all")
+    .then((res) => {
+      return res.json()
+    })
+    .then((countries) => {
+      let output = ""
+      countries.forEach((country) => {
+        output += `<option>${country.name}</option>`
+      })
+      selectCountry.innerHTML = output
+    })
+    .catch((err) => console.error(err))
+})
+
+const decrementBtn = document.querySelectorAll("#decrement")
+const quantityElem = document.querySelectorAll("#quantity")
+const incrementBtn = document.querySelectorAll("#increment")
+const priceElem = document.querySelectorAll("#price")
+const totalElem = document.querySelector("#total")
+
+for (let i = 0; i < incrementBtn.length; i++) {
+  incrementBtn[i].addEventListener("click", function () {
+    let increment = Number(this.previousElementSibling.textContent)
+    increment++
+
+    this.previousElementSibling.textContent = increment
+    totalCalc()
+  })
+
+  decrementBtn[i].addEventListener("click", function () {
+    let decrement = Number(this.nextElementSibling.textContent)
+    decrement <= 1 ? 1 : decrement--
+
+    this.nextElementSibling.textContent = decrement
+    totalCalc()
+  })
+}
+
+const totalCalc = function () {
+  const ship = 19
+  let subtotal = 0
+  let total = 0
+
+  for (let i = 0; i < quantityElem.length; i++) {
+    subtotal +=
+      Number(quantityElem[i].textContent) * Number(priceElem[i].textContent)
+  }
+
+  total = subtotal + ship
+  totalElem.textContent = total.toFixed(2)
+}
